5dcc5fd7f68df8967383c88ae6bf58cec0923781
多多 <duoduo@xiaoshijie.com>
2018-12-27 14:48:48 +0800

    update add memcached

---
+++ dependency/php/5.6/ext-shell/ext-memcached.sh
@@ -0,0 +1,17 @@
+set -e
+apt-get install libsasl2-dev cloog-ppl
+apt-get install zlib1g-dev
+mkdir -p /usr/local/src
+apt install -y wget
+cd /usr/local/src
+wget  --no-check https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+tar zxvf libmemcached-1.0.18.tar.gz
+cd libmemcached-1.0.18
+./configure --prefix=/usr/local/libmemcached --enable-sasl
+make && make install
+
+cd /usr/local/src && wget http://pecl.php.net/get/memcached-2.2.0.tgz
+tar zxvf memcached-2.2.0.tgz
+cd memcached-2.2.0 && phpize
+./configure --with-libmemcached-dir=/usr/local/libmemcached  --enable-memcached-sasl
+make && make install
