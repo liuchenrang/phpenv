aa32a47d7ff3686e33c9cc54d82401fee1201d94
多多 <duoduo@xiaoshijie.com>
2018-12-27 14:43:55 +0800

    update

--- dependency/php/7.2/ext-memcached.sh
+++
@@ -1,6 +0,0 @@
-zlib1g-dev
-apt install libmemcached-dev
-pecl download memcached-3.0.4.tgz 
-tar xvzf memcached-3.0.4.tgz 
-cd memcached-3.0.4 && phpize && ./configure && make && make install
-echo "extension=memcached.so" >  /usr/local/etc/php/conf.d/docker-php-ext-memcached.ini
